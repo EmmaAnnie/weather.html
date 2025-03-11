561b47f7e952b05056078d584c1deaeb0f6877d3
EmmaAnnie <emmabuchta06@gmail.com>
2025-03-11 12:26:47 -0600

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,2 +1,6 @@
 fetch("https://api.openweathermap.org/data/2.5/weather?q=Calgary&appid=9814d4960228c2ee15b4532e74c55719&units=metric"
     )
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data);
+    })
